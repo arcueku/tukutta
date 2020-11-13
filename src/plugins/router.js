e1f21d72ac3c6faea6bc46789421315e368271c7
arcueku <testuser@gmail.com>
2020-11-14 01:44:21 +0900

    upd:router設定変更

---
+++ src/plugins/router.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Router from "vue-router";
+import ContentDetail from "@/components/ContentDetail.vue";
+import Home from "@/components/Home.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/detail",
+      name: "detailPage",
+      component: ContentDetail,
+    },
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+  ],
+});
