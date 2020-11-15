39b76230e4e3ee3f98e3da43d6bb1faf81b7f7e6
arcueku <testuser@gmail.com>
2020-11-15 22:28:27 +0900

    vuex導入

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
-import router from "./plugins/router"; //追加
+import vuetify from "@/plugins/vuetify";
+import router from "@/plugins/router";
+import store from "@/plugins/store";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   router,
   vuetify,
   render: (h) => h(App),
